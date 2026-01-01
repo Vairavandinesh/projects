55ccdb15f40854edfe91a336fd0c79bca7b8e10a
Vairavandinesh <140646315+Vairavandinesh@users.noreply.github.com>
2026-01-01 18:52:42 +0530

    .

---
+++ console applications/Countdown.py
@@ -0,0 +1,11 @@
+#Countdown timer program in python
+#import the time module in python
+import time
+my_time=int(input("Enter time in seconds : "))
+for i in range(my_time,0,-1):
+    seconds=i%60
+    minutes=int(i/60)%60
+    hours=int(i/3600)
+    print(f"{hours:02}:{minutes:02}:{seconds:02}")
+    time.sleep(0.2)
+print("Time's UP!!!")
